c49d6741c1b89325de055cd5d1c6806a55ae3a6e
phemmati <phemmati@usfca.edu>
2020-11-14 13:43:30 -0800

    Login page Created

--- app/src/main/java/com/example/bookgraph/MainActivity.java
+++ app/src/main/java/com/example/bookgraph/MainActivity.java
@@ -1,14 +1,125 @@
 package com.example.bookgraph;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private RecyclerView postList;
+    private Toolbar mtoolBar;
+    private FirebaseAuth mAuth;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mtoolBar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mtoolBar);
+        getSupportActionBar().setTitle("Home");
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View nacView = navigationView.inflateHeaderView(R.layout.navigation_header);
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                userMenuSelector(item);
+                return false;
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+            sendUsertoLoginActivity();
+        }
+
+    }
+
+    private void sendUsertoLoginActivity() {
+
+        Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void userMenuSelector(MenuItem item){
+        switch ((item.getItemId())){
+            case R.id.nav_profile:
+                Toast.makeText(this,"profile",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_home:
+                Toast.makeText(this,"home",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_friends:
+                Toast.makeText(this,"friends list",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_find_friends:
+                Toast.makeText(this,"fine friends",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_messages:
+                Toast.makeText(this,"messages",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(this,"settings",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_Logout:
+                Toast.makeText(this,"logout",Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
