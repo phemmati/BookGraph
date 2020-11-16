594c3d9c7edf869915a265bb973c2d9a8a8ecbe1
phemmati <phemmati@usfca.edu>
2020-11-15 22:26:48 -0800

    Delete post done

--- app/src/main/java/com/example/bookgraph/ClickPostActivity.java
+++ app/src/main/java/com/example/bookgraph/ClickPostActivity.java
@@ -1,14 +1,20 @@
 package com.example.bookgraph;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -49,18 +55,29 @@ public class ClickPostActivity extends AppCompatActivity {
         clickPostRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                description = snapshot.child("description").getValue().toString();
-                image = snapshot.child("postimage").getValue().toString();
+                if(snapshot.exists()){
+                    description = snapshot.child("description").getValue().toString();
+                    image = snapshot.child("postimage").getValue().toString();
 
-                databaseUserId = snapshot.child("uid").getValue().toString();
+                    databaseUserId = snapshot.child("uid").getValue().toString();
+                    postDescription.setText(description);
+                    Picasso.get().load(image).into(postImage);
 
-                if(currentIserId.equals(databaseUserId)){
+                    if(currentIserId.equals(databaseUserId)){
 
-                    deletePostButton.setVisibility(View.VISIBLE);
-                    editPostButton.setVisibility(View.VISIBLE);
+                        deletePostButton.setVisibility(View.VISIBLE);
+                        editPostButton.setVisibility(View.VISIBLE);
+                    }
+
+                    editPostButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            editCurrentPost(description);
+                        }
+                    });
+
                 }
-                postDescription.setText(description);
-                Picasso.get().load(image).into(postImage);
 
             }
 
@@ -70,5 +87,56 @@ public class ClickPostActivity extends AppCompatActivity {
             }
         });
 
+        deletePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteCurrentPost();
+            }
+        });
+
+
+    }
+
+    private void editCurrentPost(String description) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ClickPostActivity.this);
+        builder.setTitle("Edit Post");
+
+        final EditText inputField = new EditText(ClickPostActivity.this);
+        inputField.setText(description);
+
+        builder.setView(inputField);
+        builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                clickPostRef.child("description").setValue(inputField.getText().toString());
+                Toast.makeText(ClickPostActivity.this,"Post has been updated.",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        Dialog dialog = builder.create();
+        dialog.show();
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.holo_green_dark);
+
+    }
+
+    private void deleteCurrentPost() {
+        clickPostRef.removeValue();
+        sendUserToMainActivity();
+        Toast.makeText(ClickPostActivity.this,"Pst has been deleted." ,Toast.LENGTH_LONG).show();
+
+    }
+
+    private void sendUserToMainActivity() {
+        Intent mainIntent = new Intent(ClickPostActivity.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
